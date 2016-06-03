7eac20c6da3b84804987a5ec0eb3c6f40bce3a8c
Jim Menard <jim.menard@chloeandisabel.com>
2016-06-02 22:01:09 -0400

    seq bpm calculations

--- lib/midifile/sequence.ex
+++ lib/midifile/sequence.ex
@@ -1,5 +1,7 @@
 defmodule Midifile.Sequence do
 
+  @default_bpm 120
+
   use Bitwise
 
   defstruct format: 1, division: 480,
@@ -7,9 +9,21 @@ defmodule Midifile.Sequence do
     tracks: []
 
   def name(%Midifile.Sequence{conductor_track: nil}), do: ""
-  def name(%Midifile.Sequence{conductor_track: %Midifile.Track{events: {}}}),  do: ""
+  def name(%Midifile.Sequence{conductor_track: %Midifile.Track{events: []}}),  do: ""
   def name(%Midifile.Sequence{conductor_track: %Midifile.Track{events: list}})  do
     Enum.find(list, %Midifile.Event{}, &(&1.symbol == :seq_name)).bytes
+  end
+
+  def bpm(%Midifile.Sequence{conductor_track: nil}), do: @default_bpm
+  def bpm(%Midifile.Sequence{conductor_track: %Midifile.Track{events: []}}),  do: ""
+  def bpm(%Midifile.Sequence{conductor_track: %Midifile.Track{events: list}})  do
+    tempo_event = Enum.find(list, %Midifile.Event{}, &(&1.symbol == :tempo))
+    if tempo_event do
+      microsecs_per_beat = hd(tempo_event.bytes)
+      trunc(60_000_000 / microsecs_per_beat)
+    else
+      @default_bpm
+    end
   end
 
   def ppqn(seq) do
--- test/sequence_test.exs
+++ test/sequence_test.exs
@@ -7,13 +7,18 @@ defmodule SequenceTest do
   setup do
     e = %Event{symbol: :on, delta_time: 100, bytes: [0x92, 64, 127]}
     t = %Track{events: [e, e, e]}
-    ct = %Track{events: [%Event{symbol: :seq_name, bytes: "Unnamed"}]}
+    ct = %Track{events: [%Event{symbol: :seq_name, bytes: "Unnamed"},
+                        %Event{symbol: :tempo, bytes: [trunc(60_000_000 / 82)]}]}
     {:ok, %{seq: %Sequence{division: 480, conductor_track: ct, tracks: [t, t, t]},
             track: t}}
   end
 
   test "name", context do
     assert Sequence.name(context[:seq]) == "Unnamed"
+  end
+
+  test "bpm", context do
+    assert Sequence.bpm(context[:seq]) == 82
   end
 
   test "ppqn", context do
